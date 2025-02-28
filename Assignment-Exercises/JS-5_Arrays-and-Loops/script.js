9d4bc1ece2b0b571c6f1c8d84af9e9806d083cb0
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-03-01 00:15:53 +0800

    complete JS-5_Arrays_and_Loops

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,16 @@
+const favoriteMovies = [
+  "The Matrix",
+  "The Dark Knight",
+  "Inception",
+  "Interstellar",
+  "Avatar",
+  "Avengers: Endgame",
+  "The Lord of the Rings",
+  "Avengers: Infinity War",
+  "Spider-Man: No Way Home",
+  "The Avengers",
+];
+
+for (let i = 0; i < favoriteMovies.length; i++) {
+  console.log(`Favorite Movie ${i + 1}: ${favoriteMovies[i]}`);
+}
