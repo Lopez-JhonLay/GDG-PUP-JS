655dd18be4e17df0cfe93e78578732b7b925464d
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-03-01 00:05:02 +0800

    complete JS-3_Conditional-Statements

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -1,3 +1,19 @@
 function checkAge() {
-    // write your code here 
+  // write your code here
+  let age = document.getElementById("ageInput").value;
+  let result = document.getElementById("result");
+
+  if (age < 0) {
+    document.getElementById("result").textContent =
+      "Invalid age. Please enter a valid number.";
+    return;
+  }
+
+  if (age >= 0 && age <= 12) {
+    result.textContent = "You are a child.";
+  } else if (age >= 13 && age <= 19) {
+    result.textContent = "You are a teenager.";
+  } else {
+    result.textContent = "You are an adult.";
+  }
 }
