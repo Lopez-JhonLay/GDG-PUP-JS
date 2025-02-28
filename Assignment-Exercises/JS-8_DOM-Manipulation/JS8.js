5fc341b7c54c1647cbb9d6a378d91036a246bd53
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-03-01 00:38:40 +0800

    complete JS-8_DOM-Manipulation

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,19 @@
 // your code starts here
+
+let buttonAdd = document.querySelector("#button-add");
+let buttonSubtract = document.querySelector("#button-subtract");
+let counterText = document.querySelector("#counter-text");
+
+buttonAdd.addEventListener("click", () => {
+  counterText.textContent = parseInt(counterText.textContent) + 1;
+});
+
+buttonSubtract.addEventListener("click", () => {
+  counterText.textContent = parseInt(counterText.textContent) - 1;
+
+  if (counterText.textContent < 0) {
+    counterText.textContent = 0;
+  }
+});
+
+// your code ends here
